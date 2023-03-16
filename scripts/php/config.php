1bbdd86e24dc55a046bf231d94dcb9a9538ab2b6
Eli Wilhelm <spacepopejorgle@gmail.com>
2023-03-15 20:44:11 -0500

    fixed config.php

--- scripts/php/config.php
+++ scripts/php/config.php
@@ -4,7 +4,7 @@
     define('DB_SERVER', 'fdb1029.awardspace.net');
     define('DB_USERNAME', '4240987_epwilhe');
     define('DB_PASSWORD', 'Kipper9000');
-    define('DB_NAME', 'users');
+    define('DB_NAME', '4240987_epwilhe');
     
     /* Attempt to connect to MySQL database */
     $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
