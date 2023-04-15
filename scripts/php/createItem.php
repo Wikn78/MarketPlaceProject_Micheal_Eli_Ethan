7d1008ea3e85cfb5b45a8bb4472a56aea565609a
Eli Wilhelm <spacepopejorgle@gmail.com>
2023-04-14 21:54:25 -0500

    .

--- scripts/php/createItem.php
+++ scripts/php/createItem.php
@@ -1,6 +1,6 @@
 <?php
 	header( "refresh:5; url=../../homescreen.html" ); // set this to go to the new listing eventually
-	$pic = "";
+	@require_once 'Configure.php';
 
 	if(isset($_GET['title'])) {
 		$title = ($_GET['title']);
@@ -12,23 +12,38 @@
 
 	// get user id from active session
     // if(isset($_GET['uid'])) {
-	//	$uid = ($_GET[uid]);
+
 	// }
 	$uid = 9999;
-	
+
     if(isset($_GET['location'])) {
 		$location = ($_GET['location']);
 	}
 
-    if(isset($_GET['image'])) {
-		$pic = ($_GET['image']);
+    if(isset($_FILES['image'])) {
+		$dirname = "./images/Products/";    
+
+		$of = $_FILES['image']['name'];
+		$ext = pathinfo($of, PATHINFO_EXTENSION);
+
+		$changename3 = time() * 24 * 60;
+		$image_name3 = "timage_" . $changename3 . "." . $ext;
+
+		$final_pathdir = $dirname . $image_name3;
+		$suc = move_uploaded_file($_FILES['image']['tmp_name'], "../../images/Products/");
+
+		if ($suc > 0) 
+		{
+			echo "Image uploaded successfully"; 
+		} else {
+			echo "Error : " . $_FILES['filimg1']['error'];
+		}
+		$pic = $dirname.$image_name3;
 	}
 
 	if(isset($_GET['price'])) {
 		$price = ($_GET['price']);
 	}
-
-	@require_once 'Configure.php';
 
 	if(! $conn ) {
 		die('Could not connect: ' . mysqli_error());
