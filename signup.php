41f50ca87a0662263b433d0e6a756bc77b52547f
MichaelHallq <HALLQ035@crk.umn.edu>
2023-04-17 19:17:37 -0500

    .

--- signup.php
+++ signup.php
@@ -1,11 +1,21 @@
 <?php
 // Include config file
-require_once "config.php";
+require_once "Halleium_Configure.php"; #PHP script in order to connect to the MySQL database server
  
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
- 
+$username_err = $password_err = $confirm_password_err = $email_err = "";
+$email = "";
+$address = "";
+$country = "";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    $confirm_password = trim($_POST['confirm_password']);
+    $email = trim($_POST['email']);
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
@@ -18,9 +28,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
+        if($stmt = mysqli_prepare($conn, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            }
+ 
             
             // Set parameters
             $param_username = trim($_POST["username"]);
@@ -41,7 +53,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
             // Close statement
             mysqli_stmt_close($stmt);
-        }
+        } 
     }
     
     // Validate password
@@ -63,34 +75,162 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
     
+    // Email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter a email.";
+        } elseif(strlen(trim($_POST["email"])) < 6){
+        $email_err = "Must enter valid email.";
+    } else{
+        $email = trim($_POST["email"]);
+    }
+    
+
+    
+    
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-        
+    if(!(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err))){
+        //echo"hey man";
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO AccountInformation (username, password, email, DOB, address, country) VALUES (?, ?, ?, ?, ?, ?)";
          
-        if($stmt = mysqli_prepare($link, $sql)){
+        if($stmt = mysqli_prepare($conn, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "sssdss", $param_username, $param_password, $param_email, date(DATE_RFC2822), $param_address, $param_country);
             
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_email = $email;
+            //$param_DOB = $DOB;
+            $param_address = $address;
+            $param_country = $country;
             
-            // Attempt to execute the prepared statement
+            
+         /*   // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
-                header("location: login.php");
+               header("location: login.php");
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
-            }
+            } */
 
             // Close statement
             mysqli_stmt_close($stmt);
+            
+            $url = 'Halleium_register.php';
+    
+                    echo '<script type="text/javascript">';
+                    echo'window.location.href="'.$url. '";';
+                    echo '</script>';
+                    echo '<noscript>';
+                    echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+                    echo '</noscript>'; 
+        mysqli_close($conn);
         }
     }
     
+   }
+    
     // Close connection
-    mysqli_close($link);
-}
+    mysqli_close($conn);
+    
+    
+    
+ 
 ?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <conn rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+            html {
+
+    font-family: "Lucida Sans", sans-serif;
+
+}
+
+.header {
+    color: #3B413C;
+    text-align: center;
+}
+
+.fname {
+    color: #3B413C;
+
+}
+
+.grid-container {
+    display: grid;
+    grid-template-columns: auto auto;
+    padding-left: 300px;
+    padding-right: 300px;
+}
+
+.grid-item {
+    background-color: rgba(255, 255, 255, 0.8);
+    border: 1px solid rgba(0, 0, 0, 0.8);
+    padding: 10px;
+    font-size: 30px;
+    text-align: center;
+}
+    
+    <!--
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+         -->
+    </style>
+</head>
+<form method="POST" action="">
+</form>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up for a Halleium Account</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>            
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Date of Birth</label>
+                <input type="date" name="DOB" class="form-control <?php echo (!empty($DOB_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $DOB; ?>">
+                <span class="invalid-feedback"></span>
+            </div>
+            <div class="form-group">
+                <label>Address</label>
+                <input type="text" name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $address; ?>">
+                <span class="invalid-feedback"></span>
+            </div>
+            <div class="form-group">
+                <label>Country</label>
+                <input type="text" name="country" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $country; ?>">
+                <span class="invalid-feedback"></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Already have an account? <a href="Halleium_register.php">Login here</a>.</p> <!-- change link for sign up page -->
+        </form>
+    </div>    
+</body>
+</html>
